19d5fba0f77b64d9164c669c20b853b6b549c350
Sergio2204A <217261988+Sergio2204A@users.noreply.github.com>
2025-07-30 16:08:04 -0500

    feat: :tada: quinto commit
    adicion de la funcipon main

---
+++ main.py
@@ -0,0 +1,43 @@
+from app.gestor import cargar_datos, guardar_datos, añadir_elemento, ver_elemento, buscar_elemento, editar_elemento, eliminar_elemento, elementos_categoria, guardar_y_cargar, salir
+from app import gestor
+def mostrar_menu():
+    print ('=== Coleccion Multimedia ===')
+    print ('1. añadir un  nuevo elemento')
+    print ('2. ver todos los elementos')
+    print ('3. buscar un elemento')
+    print ('4. editar un elemento')
+    print ('5. editar un elemento')
+    print ('6. ver elementos por categoria')
+    print ('7. guardar y cargar colecccion')
+    print ('8. salir')
+    print()
+
+def main ():
+    coleccion = cargar_datos()
+    while True:
+        mostrar_menu()
+        opcion = input('seleccione una opcion :')
+        print()
+
+        if opcion == "1":
+            añadir_elemento(coleccion)
+        elif opcion == "2":
+            ver_elemento(coleccion)
+        elif opcion == "3":
+            buscar_elemento(coleccion)
+        elif opcion == "4":
+            editar_elemento(coleccion)
+        elif opcion == "5":
+            eliminar_elemento(coleccion)
+        elif opcion == "6":
+            elementos_categoria(coleccion)
+        elif opcion == "7":
+            guardar_y_cargar(coleccion)
+        elif opcion == "8":
+            salir(coleccion)
+            break
+        else:
+            print('opcion invalida intenytar de nuevo')
+
+if __name__=="__main__":
+    main()
